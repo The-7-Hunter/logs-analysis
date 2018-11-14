812292f2e664389201aacedcd59bf87cdaaa74a3
Nawaf <30578987+The-7-Hunter@users.noreply.github.com>
2018-11-15 01:56:58 +0300

    following PEP8 style

--- log-analysis-nawaf/app.py
+++ log-analysis-nawaf/app.py
@@ -2,6 +2,7 @@ import psycopg2
 
 DBname = "news"
 
+
 def run_query(query):
     db = psycopg2.connect('dbname=' + DBname)
     c = db.cursor()
@@ -23,7 +24,6 @@ def top_articles():
     """
     result = run_query(query)
     print('\n1. What are the most popular three articles of all time?\n')
-  
     for i in result:
         title = i[0]
         label = '" with ' + str(i[1]) + " views"
@@ -49,10 +49,12 @@ def top_authors():
     for i in result:
         print('- ' + i[0] + ' with ' + str(i[1]) + " views")
 
+
 def error_days():
     query = """
         SELECT total.day,
-          ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
+          ROUND(((errors.error_requests*1.0) / total.requests), 3)
+          AS error_pers
         FROM (
           SELECT date_trunc('day', time) "day", count(*) AS error_requests
           FROM log
@@ -65,20 +67,22 @@ def error_days():
           GROUP BY day
           ) AS total
         ON total.day = errors.day
-        WHERE (ROUND(((errors.error_requests*100.0) / total.requests), 2) > 1.00)
-        ORDER BY percent DESC;
+        WHERE (ROUND(((errors.error_requests*100.0) / total.requests), 2)
+         > 1.00)
+        ORDER BY error_pers DESC;
     """
 
     result = run_query(query)
     print('\n3. On which days did more than 1% of requests lead to errors?\n')
     for i in result:
         date = i[0].strftime('%B %d, %Y')
-        errors = str(round(i[1]*100,1)) + "%" + " errors"
+        errors = str(round(i[1]*100, 1)) + "%" + " errors"
         print('\t' + date + " -- " + errors)
         print
 
+
 print('\t---------------------------------')
-print('\t     Excuting Please Wait        ')
+print('\t     Executing Please Wait        ')
 print('\t---------------------------------')
 top_articles()
 top_authors()
